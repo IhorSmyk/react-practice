6e9ae404da360357bfbe18b7451aeb94cd5e1f52
romfantast <romfantast@gmail.com>
2022-12-22 16:06:09 +0200

    This is a new commit for what I originally planned to be amended

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
-import Header from "./Header/Header";
+import Header from "../Header/Header";
+import { TransactionForm } from "../TransactionForm/TransactionForm";
 
 const MainPage = () => {
   return (
     <>
       <Header />
+      <TransactionForm />
     </>
   );
 };

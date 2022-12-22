96fb344a9c311edb3be7e42a9301b18ef027c740
Dima Telechkun <telechkun93@gmail.com>
2022-12-22 17:16:36 +0200

    update btn

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import { TransactionForm } from "../TransactionForm/TransactionForm";
+import { TransactionNavButtons } from "../TransactionNavButtons/TransactionNavButtons";
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <TransactionForm />
+      <TransactionNavButtons />
     </>
   );
 };
